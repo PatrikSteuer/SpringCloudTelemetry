136fb2d75534e7d3305575df2621dbdda5520c0c
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2020-12-04 21:57:21 +0100

    added an HTTP filter which fills the MDC with service and tenant information.

---
+++ sample-service-c/src/main/java/com/equalities/cloud/service/LogEnhancerFilter.java
@@ -0,0 +1,32 @@
+package com.equalities.cloud.service;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * A class showing the use of Slf4J's Mapped Diagnostic Context
+ * to enhance logs written by an application with context-specific
+ * information. This can be static information about the service itself, e.g.
+ * to be able to filter logs later by service ID, or dynamic information
+ * like a tenant ID.
+ */
+@Component
+public class LogEnhancerFilter implements Filter {
+
+    @Override
+    public void destroy() {
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        MDC.put("service", "Service-C");
+        MDC.put("tenant", "SomeTenantID");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
